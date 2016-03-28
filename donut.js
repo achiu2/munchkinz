b7267adf6f80e875955be0f7242c32780fb83eec
JeffreyZou13 <jeffreyzou13@gmail.com>
2016-03-28 10:59:04 -0400

    some more stuff

--- donut.js
+++ donut.js
@@ -7,12 +7,13 @@
 var WIDTH = 860;
 var HEIGHT = 600;
 var radius = Math.min(WIDTH, HEIGHT) / 2;
+var data;
+var colors;
 
 //Initialize the svgs
-var init = function() {
-  var svg = d3.select('body')
-    .append('svg')
-    .append('g');
+var svg = d3.select('body')
+  .append('svg')
+  .append('g');
 
   svg.append('g')
   .attr('class', 'slice');
@@ -20,6 +21,28 @@ var init = function() {
   .attr('class', 'labels');
   svg.append('g')
   .attr('class', 'lines');
-}
+
+svg.attr('transform', 'translate(' + WIDTH/2 + ',' + HEIGHT/2 + ')');
+
+var pie = d3.layout.pie()
+  .value(function(d) {
+    return d.value;
+  });
+
+var color = d3.scale.ordinal()
+  .domain(data)
+  .range(colors);
+
+//Return the next set of data based on an index
+var makeSubData = function() {
+  //TODO
+};
+
+change(makeSubData);
+
+//Add selector for the bar to scroll through the years
 
 //Make a pie
+function change(subData) {
+  //Change the data represented on the donut graph based on a subset of the data
+};
